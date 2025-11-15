97a146917366866d646d8116b724462f507657af
bernardo0921 <ephraimbernard7@gmail.com>
2025-11-15 00:08:25 +0000

    added my new render domain to allowed hosts

--- backend_ai/settings.py
+++ backend_ai/settings.py
@@ -32,7 +32,7 @@ GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,192.168.100.7').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,192.168.100.7,agriguide-backend-79j2.onrender.com').split(',')
 
 
 # Application definition
@@ -86,6 +86,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'https://agriguide-backend-79j2.onrender.com'
     # Add your Flutter app's origin
 ]
 
