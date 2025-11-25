33dca8080d12eedac218d090498b5627bf6a7c81
bernardo0921 <ephraimbernard7@gmail.com>
2025-11-25 01:52:48 +0000

    Fix migration issues

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Fix the partial migration
-python manage.py fix_migrations
+# Fix the database
+python manage.py fix_db
 
-# Run migrations
+# Run migrations fresh
 python manage.py migrate
+
+# Create superuser if needed
+# python manage.py createsuperuser --noinput || true
