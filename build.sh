10919111328c766d7a851aba56848c4f32aa3873
bernardo0921 <ephraimbernard7@gmail.com>
2025-11-25 01:22:33 +0000

    I know what I did

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations agriguide_ai 006
 python manage.py migrate agriguide_ai 0006
 python manage.py migrate 
